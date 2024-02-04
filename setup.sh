3b1d8842594d9935376de7dd52c40efad802d336
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2024-02-04 16:15:13 +0000

    refactor: shell function for DRY

--- setup.sh
+++ setup.sh
@@ -5,33 +5,19 @@ set -eux
 mkdir -p Datasets/coco/images
 cd Datasets/coco/images
 
-curl http://images.cocodataset.org/zips/train2017.zip --output train2017.zip
-curl http://images.cocodataset.org/zips/val2017.zip --output val2017.zip
-curl http://images.cocodataset.org/zips/test2017.zip --output test2017.zip
-curl http://images.cocodataset.org/zips/unlabeled2017.zip --output unlabeled2017.zip
+get_data() {
+    curl "$1" --output "$2"
+    unzip "$2"
+    rm "$2"
+}
 
-unzip train2017.zip
-unzip val2017.zip
-unzip test2017.zip
-unzip unlabeled2017.zip
-
-rm train2017.zip
-rm val2017.zip
-rm test2017.zip
-rm unlabeled2017.zip
+get_data "http://images.cocodataset.org/zips/train2017.zip" "train2017.zip"
+get_data "http://images.cocodataset.org/zips/val2017.zip" "val2017.zip"
+get_data "http://images.cocodataset.org/zips/test2017.zip" "test2017.zip"
+get_data "http://images.cocodataset.org/zips/unlabeled2017.zip" "unlabeled2017.zip"
 
 cd ../
-curl http://images.cocodataset.org/annotations/annotations_trainval2017.zip --output annotations_trainval2017.zip
-curl http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip --output stuff_annotations_trainval2017.zip
-curl http://images.cocodataset.org/annotations/image_info_test2017.zip --output image_info_test2017.zip
-curl http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip --output image_info_unlabeled2017.zip
-
-unzip annotations_trainval2017.zip
-unzip stuff_annotations_trainval2017.zip
-unzip image_info_test2017.zip
-unzip image_info_unlabeled2017.zip
-
-rm annotations_trainval2017.zip
-rm stuff_annotations_trainval2017.zip
-rm image_info_test2017.zip
-rm image_info_unlabeled2017.zip
+get_data "http://images.cocodataset.org/annotations/annotations_trainval2017.zip" "annotations_trainval2017.zip"
+get_data "http://images.cocodataset.org/annotations/stuff_annotations_trainval2017.zip" "stuff_annotations_trainval2017.zip"
+get_data "http://images.cocodataset.org/annotations/image_info_test2017.zip" "image_info_test2017.zip"
+get_data "http://images.cocodataset.org/annotations/image_info_unlabeled2017.zip" "image_info_unlabeled2017.zip"
